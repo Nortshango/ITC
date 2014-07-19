de8a2337b82629caad915a200e4331896bb487ba
Nortshango <nortshango@gmx.de>
2014-07-19 15:37:52 +0200

    test

--- ITC/DBUtil.cs
+++ ITC/DBUtil.cs
@@ -14,7 +14,8 @@ namespace ITC
 
         public DBUtil()
         {
-            /*var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
+            
+            var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
 
             var uri = new Uri(uriString);
             connectionString = new SqlConnectionStringBuilder
@@ -23,24 +24,15 @@ namespace ITC
                 InitialCatalog = uri.AbsolutePath.Trim('/'),
                 UserID = uri.UserInfo.Split(':').First(),
                 Password = uri.UserInfo.Split(':').Last(),
-            }.ConnectionString;*/
-
-            connectionString = new SqlConnectionStringBuilder
-            {
-                DataSource = "86e95deb-3cb3-4729-80f7-a26600bf1e84.sqlserver.sequelizer.com",
-                InitialCatalog = "db86e95deb3cb3472980f7a26600bf1e84",
-                UserID = "gvhdololevszhtrr",
-                Password = "VcVdrpbVe5vFDpRVwKNSdQkUJL8Te5yNG7kecFDY7ar8LRiTNeCk8SMfZbAMppRh",
             }.ConnectionString;
-
 
         }
 
         public DataSet GetUser()
         {
-            string query = "SELECT * FROM \"User\"";
+            string query = "SELECT * FROM TEST";
             SqlCommand cmd = new SqlCommand(query);
-            return FillDataSet(cmd, "User");
+            return FillDataSet(cmd, "TEST");
         }
 
         private DataSet FillDataSet(SqlCommand cmd, string tableName)
